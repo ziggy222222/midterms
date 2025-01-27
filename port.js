b01bef2c9d79e5e5ad283974975ffcacb18ea74e
ziggy222222 <renoirrdelrosario409@gmail.com>
2025-01-27 19:15:34 +0800

    port com 1

--- port.js
+++ port.js
@@ -32,38 +32,5 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-
-    // typing text animation script
-    /*var typed = new Typed(".typing", {
-        strings: ["hotdog", "#","#", "#", "Freelance"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true 
-    });*/
-
-    /*var typed = new Typed(".typing-2", { strings: ["hi", "#", "#", "#", "Freelance"], typeSpeed: 100, backSpeed: 10, loop: true});
-    */
-    // owl carousel script
-    /*$('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-     });*/
 });
 
